ddbb0a14c735e4d1b3d6a2cb11d6199199a6c9e5
Manfred Marcano <manfredalberto@gmail.com>
2024-07-13 21:11:24 -0400

    2nd commit

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
+      {/* <img src="/logo.png" alt="A server surrounded by magic sparkles." /> */}
+      <h1 style={{paddingTop: '20%'}}>🔥 Saturday Night 🔥</h1>
     </main>
   );
 }
